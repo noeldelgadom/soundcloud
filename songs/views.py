8d5aad27d257c16a839936617abc0beec0f17aa3
Noel Delgado <noeldelgadom@gmail.com>
2017-05-08 10:14:13 -0500

    added home url and home/songs

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import songs.templates
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def index(request):
+    return render(request, 'index.html')
